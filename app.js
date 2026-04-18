3861a562098bf8b09f6227a39c237b858a9f7a34
Christopher Joiner <cjoiner9656@gmail.com>
2026-04-18 18:13:34 -0400

    updated constructor function and added block function

--- app.js
+++ app.js
@@ -1,14 +1,56 @@
+const SHA256 = require("crypto-js/sha256");
+
 class Block {
   constructor(timestamp, data) {
     this.index = 0;
     this.timestamp = timestamp;
     this.data = data;
     this.previousHash = "o";
-    this.hash = this.caluclateHash();
+    this.hash = this.calculateHash();
     this.nonce = o;
   }
 
-  caluclateHash() {}
+  calculateHash() {
+    return SHA256(
+      this.index + this.previousHash + this.timestamp + this.data + this.nonce,
+    ).toString();
+  }
 
   mineBlock(difficulty) {}
+}
+
+class Blockchain {
+  constructor() {
+    this.chain = [this.createGenesis()];
+  }
+
+  createGenesis() {
+    return new Block(o, "01/01/2017", "Genesis block", "o");
+  }
+
+  latestBlock() {
+    return this.chain[this.chain.length - 1];
+  }
+
+  addBlock(newBlock) {
+    newBlock.previousHash = this.latestBlock().hash;
+    newBlock.hash = newBlock.calculateHash();
+    this.chain.push(newBlock);
+  }
+
+  checkVaild() {
+    for (let i = 1; i < this.chain.length; i++) {
+      const currentBlock = this.chain[i];
+      const previousBlock = this.chain[i - 1];
+
+      if (currentBlock.hash !== currentBlock.calculateHash()) {
+        return false;
+      }
+
+      if (currentBlock.previousHash !== previousBlock.hash) {
+        return false;
+      }
+    }
+    return true;
+  }
 }
